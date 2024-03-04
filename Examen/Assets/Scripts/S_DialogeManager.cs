47651861c1fb6dfc298b2e073721aad701bacf75
Ruben Stienstra <ruben.stienstra@icloud.com>
2024-03-04 14:55:56 +0100

    De DialogeManager is nu gemakkelijker en beter te gebruiken.
    Voor artists.

--- Examen/Assets/Scripts/S_DialogeManager.cs
+++ Examen/Assets/Scripts/S_DialogeManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine.Events;
 
 public class S_DialogeManager : MonoBehaviour
 {
-    public List<GameObject> dialoges;
+    public List<Dialoge> dialoges = new List<Dialoge>();
     public UnityEvent onDialogeCompleted;
     public Animator animationCutscene;//Add Diagram
 
@@ -13,16 +14,36 @@ public class S_DialogeManager : MonoBehaviour
     public bool dialogeEnabled;
     public bool allowDebug;//Add Diagram
     private int crDialoge;
+    private int crDialogeFrame;
 
-    public void StartDialoge()
+    [Serializable]
+    public class Dialoge 
+    {
+        public List<DialogeFrame> dialogeFrames = new List<DialogeFrame>(2);
+
+        [Serializable]
+        public class DialogeFrame
+        {
+            public List<GameObject> gameObjectsToChange;
+        }
+    }
+
+    
+
+    public void StartDialoge(int dialogeNumber)
     {
         if (dialogeEnabled)
         {
-            Debug.LogWarning("Dialoge has already started!");
+            Debug.LogWarning("Dialoge has already started!", this);
             return;
         }
+        crDialoge = dialogeNumber;
         dialogeEnabled = true;
-        animationCutscene.SetBool("CutScene", false);
+        if (!animationCutscene)
+        {
+            Debug.LogWarning("Variable animationCutscene is not attached to the script!", this);
+        }
+        animationCutscene.SetBool("Cutscene", true);//Elke dialoge begint met een animatie.
 
         SkipNextDialoge();
     }
@@ -34,40 +55,56 @@ public class S_DialogeManager : MonoBehaviour
         }
         if (allowDebug)
         {
-            print("DialogeNumber Before: " + crDialoge);
+            print("DialogeNumber Before: " + crDialogeFrame);
         }
 
-        if (crDialoge >= dialoges.Count)//Als het de laatste dialoge is.
+        if (crDialogeFrame >= dialoges[crDialoge].dialogeFrames.Count)//Als het de laatste dialoge is.
         {
             EndDialoge();
             return;
         }
         else
         {
-            if(crDialoge -1 >= 0)
+            if(crDialogeFrame -1 >= 0)//Disables de vorige dialoge.
             {
-                dialoges[crDialoge -1].SetActive(false);
+                for (int i = 0; i < dialoges[crDialoge].dialogeFrames[crDialogeFrame - 1].gameObjectsToChange.Count; i++)
+                {
+                    dialoges[crDialoge].dialogeFrames[crDialogeFrame - 1].gameObjectsToChange[i].SetActive(false);
+                }
             }
-            if(crDialoge <= dialoges.Count)
+            if(crDialogeFrame <= dialoges[crDialoge].dialogeFrames.Count)//Enables de volgende dialoge.
             {
-                dialoges[crDialoge].SetActive(true);
+                for (int i = 0; i < dialoges[crDialoge].dialogeFrames[crDialogeFrame].gameObjectsToChange.Count; i++)
+                {
+                    dialoges[crDialoge].dialogeFrames[crDialogeFrame].gameObjectsToChange[i].SetActive(true);
+                }
             }
-            crDialoge++;
+            crDialogeFrame++;
         }
 
         if (allowDebug)
         {
-            print("DialogeNumber After: " + crDialoge);
+            print("DialogeNumber After: " + crDialogeFrame);
         }
     }
     public void EndDialoge()
     {
         dialogeEnabled = false;
-        animationCutscene.SetBool("CutScene",true);
-        dialoges[dialoges.Count -1].SetActive(false);
-        crDialoge = 0;
+        animationCutscene.SetBool("Cutscene", false);//Elke dialoge eindigt met een animatie.
+
+        for (int i = 0; i < dialoges[crDialoge].dialogeFrames[crDialogeFrame - 1].gameObjectsToChange.Count; i++)//Veranderd alle GameObjects naar false in een dialoge.
+        {
+            dialoges[crDialoge].dialogeFrames[dialoges[crDialoge].dialogeFrames.Count - 1].gameObjectsToChange[i].SetActive(false);
+        }
+
+        if (allowDebug)
+        {
+            print("DialogeFrames done on: " + crDialogeFrame);
+        }
+        crDialogeFrame = 0;
 
         onDialogeCompleted.Invoke();
     }
+        
 
 }
