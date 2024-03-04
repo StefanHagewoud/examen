70744017790eb14418f94c53db29dbff63eedc7e
Ruben Stienstra <ruben.stienstra@icloud.com>
2024-03-04 15:51:41 +0100

    Het FunctionActivatorTrigger script heeft nu meer functionaliteit
    Je kan nu: maximaal aantal keren laten activeren en laten disablen wanneer het maximaal aantal keer geactiveerd is.

--- Examen/Assets/Scripts/S_FunctionActivatorTrigger.cs
+++ Examen/Assets/Scripts/S_FunctionActivatorTrigger.cs
@@ -5,20 +5,44 @@ using UnityEngine.Events;
 
 public class S_FunctionActivatorTrigger : MonoBehaviour
 {
+    //Dit script heeft Ruben gemaakt, omdat de artists het nodig hadden.
+    public int timesCanActivate = -1;
+    public bool disableWhenMaxActivated = true;
     public string tagToLookFor;
     public UnityEvent OnTriggered;
+
     private void OnTriggerEnter(Collider other)
     {
         if(tagToLookFor != "")
         {
             if (other.transform.CompareTag(tagToLookFor))
             {
-                OnTriggered.Invoke();
+                ActivateTrigger();
             }
         }
         else
         {
+            ActivateTrigger();
+        }
+    }
+    private void ActivateTrigger()
+    {
+        //Checking if player allowed to activate
+        if(timesCanActivate < 0)
+        {
+            OnTriggered.Invoke();
+            return;
+        }
+
+        if (timesCanActivate > 0)
+        {
+            timesCanActivate--;
             OnTriggered.Invoke();
         }
+        if (disableWhenMaxActivated && timesCanActivate == 0)
+        {
+            gameObject.SetActive(false);
+        }
+
     }
 }
