2226023bcefc1fc0ed91f146c57d875f444f1d16
Ruben Stienstra <ruben.stienstra@icloud.com>
2024-03-05 11:05:14 +0100

    CrateSpawner script werkt, behalve 1 ding.
    De speler moet er nog tegen aan kunnen rollen om de crate open te kunnen maken.

--- Examen/Assets/Scripts/S_CrateSpawner.cs
+++ Examen/Assets/Scripts/S_CrateSpawner.cs
@@ -3,27 +3,50 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class S_CrateSpawner : MonoBehaviour
 {
-    public GameObject cratePrefab;
-    public List<Transform> crateSpawnLocations;
-    public Vector3 maximumSpawnRange;//Add Diagram
+    public UnityEvent OnCrateSpawned;
 
+    public GameObject medkitPowerupPrefab;//Add Diagram
+    public GameObject shieldPowerupPrefab;//Add Diagram
+    public GameObject rpgPowerupPrefab;//Add Diagram
+    public List<GameObject> weaponsPrefab;//Add Diagram
+    
     [Header("Debug")]
     public bool allowDebug;
 
-    public void SpawnCrate(int listNumber)
+    public void SpawnCrate()
     {
+        int randomPowerupInt = Random.Range(0, 4);
+        GameObject prefabToInstantiate = new();
+        switch (randomPowerupInt)
+        {
+            case 0:
+                prefabToInstantiate = medkitPowerupPrefab;
+                break;
+            case 1:
+                prefabToInstantiate = shieldPowerupPrefab;
+                break;
+            case 2:
+                prefabToInstantiate = rpgPowerupPrefab;
+                break;
+            case 3://For every weapon
+                int randomWeaponInt = Random.Range(0, weaponsPrefab.Count);
+                prefabToInstantiate = weaponsPrefab[randomWeaponInt];
+                break;
+            default:
+                UnityEngine.Debug.LogWarning("Nummer is out of range!", this);
+                break;
+        }
 
+        Instantiate(prefabToInstantiate, transform.position, Quaternion.identity);
 
-        Instantiate(cratePrefab, maximumSpawnRange, Quaternion.identity, crateSpawnLocations[listNumber]);
-    }
-    private void OnDrawGizmos()
-    {
+        OnCrateSpawned.Invoke();
         if (allowDebug)
         {
-            Gizmos.DrawWireCube(transform.position, maximumSpawnRange);
+            print("Powerup or Weapon is instantiated!");
         }
     }
 }
