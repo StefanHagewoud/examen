55f36e4c40fcab46280acfd4eaa7785445669189
Warner Boonstra <97068907@st.deltion.nl>
2024-03-06 10:52:56 +0100

    Start Cutscenes linked
    Als je op game start clickt begint de game en krijg je de eerste cutscene.

--- Examen/Assets/UI/Main Menu/S_Game_Start.cs
+++ Examen/Assets/UI/Main Menu/S_Game_Start.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class S_Game_Start : MonoBehaviour
 {
     public Animator mainMenu;
+    public Animator auto1Cutscene;
+    public Animator auto2Cutscene;
+    public Animator autoPolitieCutscene;
+
 
     public void GameStart()
     {
         mainMenu.SetTrigger("GameStart");
+        auto1Cutscene.SetTrigger("Auto Stop");
+        auto2Cutscene.SetTrigger("Auto Stop");
+        autoPolitieCutscene.SetTrigger("IntroStart");
     }
 }
