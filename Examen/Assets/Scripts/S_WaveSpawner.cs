11b06ef314d94785f8337143348d7e3ba9432863
Jos Rigterink <rigterinkjos@gmail.com>
2024-03-07 10:48:34 +0100

    wave spawner & enemy fixes

--- Examen/Assets/Scripts/S_WaveSpawner.cs
+++ Examen/Assets/Scripts/S_WaveSpawner.cs
@@ -7,7 +7,7 @@ public class S_WaveSpawner : MonoBehaviour
     [SerializeField]
     private List<Transform> spawnPoints;
     public Wave[] waves;
-    public int wavesCount = 0;
+    private int wavesCount = 0;
     [SerializeField]
     private float waveInterval;
     private float countdown = 0f;
