1e4fe5b81564a186f32efdab74a85e7a7bce37a7
Jos Rigterink <rigterinkjos@gmail.com>
2024-03-11 16:55:35 +0100

    Boss Enemy Update
    When boss is half hp or under he will switch state and come closer

--- Examen/Assets/Scripts/S_Enemy.cs
+++ Examen/Assets/Scripts/S_Enemy.cs
@@ -25,13 +25,24 @@ public class S_Enemy : MonoBehaviour
     private GameObject bulletPrefab;
     [SerializeField]
     private bool melee;
+    private bool rocketExplosion;
     [SerializeField]
     private bool boss;
     public bool passive;
+    public bool rocketLauncher;
     [SerializeField]
     private GameObject bloodParticle;
     [SerializeField]
     private GameObject muzzleFlashParticle;
+    [SerializeField]
+    private GameObject explosionEffect;
+    private float timer;
+
+    [Header("Rocket Launcher Settings")]
+    public GameObject rocketPrefab;
+    private GameObject rocket;
+    private bool firedRocket;
+    private bool rocketExploding;
 
     void Start()
     {
@@ -61,6 +72,60 @@ public class S_Enemy : MonoBehaviour
             stopRange = 12f;
         }
         ChasePlayer();
+        if (rocketLauncher)
+        {
+            if (firedRocket)
+            {
+                if (!rocketExploding)
+                {
+                    rocket.GetComponent<Rigidbody>().velocity = rocket.transform.forward * 10;
+                }
+                else
+                {
+                    rocket.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                }
+                timer += Time.deltaTime;
+
+                Collider[] colliders = Physics.OverlapSphere(rocket.transform.position, 0.1f);
+
+                foreach (Collider nearbyObject in colliders)
+                {
+                    // Apply explosion force to rigidbodies
+                    if (!nearbyObject.CompareTag("Enemy"))
+                    {
+                        rocketExploding = true;
+                        //GameObject.Instantiate(currentGunData.rocketExplodeAnim, rocket.transform.position, rocket.transform.rotation);
+                        Collider[] hits = Physics.OverlapSphere(rocket.transform.position, 10);
+                        foreach (Collider hit in hits)
+                        {
+                            Debug.Log(hit.name + " hit by explosion");
+                            GameObject explosion = Instantiate(explosionEffect, rocket.transform.position, rocket.transform.rotation);
+                            Destroy(explosion, 1f);
+                            if (hit.TryGetComponent<Rigidbody>(out Rigidbody hitRB))
+                            {
+                                hitRB.AddExplosionForce(1000, rocket.transform.position, 10);
+                                // force, position, radius
+                            }
+                            if (hit.GetComponent<S_Player>())
+                            {
+                                Debug.Log(hit.name + "has been hit");
+                                hit.GetComponent<S_Player>().TakeDamage(damage);
+                            }
+                        }
+                        Destroy(rocket);
+                        firedRocket = false;
+                        rocketExploding = false;
+                    }
+                }
+                if (timer >= 5)
+                {
+                    Destroy(rocket);
+                    firedRocket = false;
+                    rocketExploding = false;
+                    timer = 0f;
+                }
+            }
+        }
     }
 
     public void ChasePlayer()
@@ -149,7 +214,7 @@ public class S_Enemy : MonoBehaviour
         if(boss && health <= maxHealth / 2)
         {
             enemyAgent.isStopped = false;
-            stopRange = 15f;
+            stopRange = 12f;
         }
         if (health <= 0)
         {
@@ -159,19 +224,31 @@ public class S_Enemy : MonoBehaviour
 
     public void Attack(float damage)
     {
-        if (melee)
+        if (rocketLauncher)
         {
-            Melee(damage);
-        }
-        else
-        {
+            GameObject rocket = Instantiate(rocketPrefab, transform.GetChild(2).position, transform.rotation);
+            rocket.GetComponent<Rigidbody>().AddForce(rocket.transform.forward * 200f);
             GameObject muzzleFlash = Instantiate(muzzleFlashParticle, transform.GetChild(2).position, transform.rotation);
             Destroy(muzzleFlash, 1f);
-            GameObject bullet = Instantiate(bulletPrefab, transform.GetChild(2).position, transform.rotation);
-            bullet.GetComponent<S_Bullet>().damage = damage;
-            bullet.GetComponent<S_Bullet>().host = gameObject;
-            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 500f);
-            Destroy(bullet, 3f);
+        }
+        if (!rocketLauncher)
+        {
+            if (melee)
+            {
+                Melee(damage);
+            }
+            else
+            {
+                GameObject muzzleFlash = Instantiate(muzzleFlashParticle, transform.GetChild(2).position, transform.rotation);
+                Destroy(muzzleFlash, 1f);
+                GameObject bullet = Instantiate(bulletPrefab, transform.GetChild(2).position, transform.rotation);
+                bullet.GetComponent<S_Bullet>().damage = damage;
+                bullet.GetComponent<S_Bullet>().host = gameObject;
+
+                bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 500f);
+
+                Destroy(bullet, 3f);
+            }
         }
     }
     public void Melee(float damage)
